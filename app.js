6da257ef13f65e76dbf22c952a00f88b0b545c50
DevGleicekelly <gleicekellym1@outlook.com>
2025-03-17 19:47:15 -0300

    Adiciona função para capturar e adicionar amigos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let amigos = [];
 
 function adicionarAmigo() {
+    "Ana, Maria ,Bruna,Joice";
     // Captura o valor do campo de entrada
     let campoEntrada = document.getElementById("nomeAmigo");
     let nome = campoEntrada.value;
@@ -12,9 +13,28 @@ function adicionarAmigo() {
         // Atualiza o array de amigos
         amigos.push(nome);
         console.log(amigos); // Para verificar se o nome foi adicionado
+        exibirAmigos();
 
         // Limpa o campo de entrada
         campoEntrada.value = "";
+    }
+}
+
+function exibirAmigos() {
+    // Obter o elemento da lista
+    let lista = document.getElementById("Ana, Maria ,Bruna,Joice");
+
+    // Limpar a lista existente
+    lista.innerHTML = "";
+
+    // Percorrer o array de amigos
+    for (let i = 0; i < amigos.length; i++) {
+        // Criar um novo elemento de lista <li>
+        let item = document.createElement("li");
+        item.textContent = amigos[i]; // Adicionar o nome do amigo ao <li>
+
+        // Adicionar o <li> à lista
+        lista.appendChild(item);
     }
 }
 
